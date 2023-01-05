01acadd6e6145204821c1eca67b8d5cd275dffa3
Maximilian Sieber <101207002+zero048@users.noreply.github.com>
2023-01-05 10:39:25 +0100

    Create java files

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
